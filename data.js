a707018e37820084b85d83aea4550677f349df98
sosaskze143 <sosaskze143@gmail.com>
2025-04-25 13:53:26 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,83 @@
+const shadowCurrency = [
+  {
+    serial: "ZH-13-143572",
+    owner: "فلان 1",
+    rank: "نادر",
+    issued: "أبريل 2025",
+    description: "لشجاعته في الموقف الفلاني.",
+    status: "ثابتة",
+    lastUpdated: "أبريل 2025"
+  },
+  {
+    serial: "ZH-10-143975",
+    owner: "فلان 2",
+    rank: "ذهبي",
+    issued: "أبريل 2025",
+    description: "لوقوفه مع صديقه في وقت الحاجة.",
+    status: "قابلة للتحويل",
+    lastUpdated: "أبريل 2025"
+  },
+  {
+    serial: "ZH-11-143179",
+    owner: "فلان 3",
+    rank: "أسطوري",
+    issued: "أبريل 2025",
+    description: "لطيبته المتناهية.",
+    status: "ثابتة",
+    lastUpdated: "أبريل 2025"
+  },
+  {
+    serial: "ZH-14-143455",
+    owner: "فلان 4",
+    rank: "فضي",
+    issued: "أبريل 2025",
+    description: "لتفانيه في العمل.",
+    status: "قابلة للتحويل",
+    lastUpdated: "أبريل 2025"
+  },
+  {
+    serial: "ZH-10-143431",
+    owner: "فلان 5",
+    rank: "نادر",
+    issued: "أبريل 2025",
+    description: "لحكمته وهدوئه في الأزمات.",
+    status: "ثابتة",
+    lastUpdated: "أبريل 2025"
+  },
+  {
+    serial: "ZH-11-143575",
+    owner: "فلان 6",
+    rank: "ذهبي",
+    issued: "أبريل 2025",
+    description: "لوقفة لا تُنسى.",
+    status: "ثابتة",
+    lastUpdated: "أبريل 2025"
+  },
+  {
+    serial: "ZH-10-143535",
+    owner: "فلان 7",
+    rank: "فضي",
+    issued: "أبريل 2025",
+    description: "لدعمه المستمر للكل.",
+    status: "قابلة للتحويل",
+    lastUpdated: "أبريل 2025"
+  },
+  {
+    serial: "ZH-16-143783",
+    owner: "فلان 8",
+    rank: "أسطوري",
+    issued: "أبريل 2025",
+    description: "لإلهامه لمن حوله.",
+    status: "ثابتة",
+    lastUpdated: "أبريل 2025"
+  },
+  {
+    serial: "ZH-16-143194",
+    owner: "فلان 9",
+    rank: "نادر",
+    issued: "أبريل 2025",
+    description: "لإخلاصه في كل شيء.",
+    status: "قابلة للتحويل",
+    lastUpdated: "أبريل 2025"
+  }
+];
